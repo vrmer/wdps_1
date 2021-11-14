6bb98a425c9586fbc229330ba6ca0155ad5e3b30
vrmer <marcell.fekete95@gmail.com>
2021-11-14 19:13:15 +0100

    added filtering for patterns with bs4

--- z_bs4.py
+++ z_bs4.py
@@ -22,6 +22,14 @@ target = 50_000
 regex_pattern = re.compile('(^WARC-.*)|(^(X-Crawler-)?Content-.*)')
 # javascript = {'=', ';'}
 
+patterns = set()
+
+with open('patterns.txt') as infile:
+    lines = infile.readlines()
+    for line in lines:
+        pattern = line.strip()
+        patterns.add(pattern)
+
 
 def collect_entities(line):
     """
@@ -45,61 +53,65 @@ skip = False
 all_entities = []
 
 with gzip.open(path, 'rt', errors='ignore', encoding='utf8') as stream:
-    with tqdm(total=target, desc='Iterating: ') as pbar:
-        for idx, line in enumerate(stream):
-            # print(idx)
+    # with tqdm(total=target, desc='Iterating: ') as pbar:
+    for idx, line in enumerate(stream):
+        # print(idx)
 
-            # skip from True to False
-            if skip is True:
-                if 'WARC/1.0' in line:
-                    skip = False
-                elif 'Content-Type: text/html' in line:
-                    skip = False
-                else:
-                    pass
-
+        # skip from True to False
+        if skip is True:
+            if 'WARC/1.0' in line:
+                skip = False
+            elif 'Content-Type: text/html' in line:
+                skip = False
             else:
-                # skip from False to True
-                if 'Content-Type: ' in line and 'text/html' not in line:
-                    skip = True
+                pass
+
+        else:
+            # skip from False to True
+            if 'Content-Type: ' in line and 'text/html' not in line:
+                skip = True
+            else:
+                # try to skip lines associated with the crawl itself
+                match = re.search(regex_pattern, line)
+                if match:
+                    continue
                 else:
-                    # try to skip lines associated with the crawl itself
-                    match = re.search(regex_pattern, line)
-                    if match:
+                    # process candidate results
+                    soup = BeautifulSoup(line, features='html5lib')
+                    if soup.style:
                         continue
-                    else:
-                        # process candidate results
-                        soup = BeautifulSoup(line, features='html5lib')
-                        if soup.style:
-                            continue
-                        elif soup.script:
-                            continue
-                        elif soup.body:
+                    elif soup.script:
+                        continue
+                    elif soup.body:
+                        try:
+                            text = soup.body.get_text()
                             try:
-                                text = soup.body.get_text()
-                                try:
-                                    languages = lang_det.predict(text)
-                                    # stuff = f'{idx} + {languages}'
-                                    # print(stuff)
-                                    # print(languages)
-                                    if '__label__en' in languages[0]:
-                                        entity_list = collect_entities(text)
-                                        if entity_list:
-                                            # print(entity_list)
-                                            all_entities.append(entity_list)
-                                except:
-                                    continue
+                                languages = lang_det.predict(text)
+                                # stuff = f'{idx} + {languages}'
+                                # print(stuff)
+                                # print(languages)
+                                if '__label__en' in languages[0]:
+                                    # if len(text.strip()) >= 4 and ' ' in text and '||' not in text:
+                                    if len(text.strip()) >= 4:
+                                        if text.strip() not in patterns:
+                                            print(text)
+                                    # entity_list = collect_entities(text)
+                                    # if entity_list:
+                                        # print(entity_list)
+                                        # all_entities.append(entity_list)
                             except:
                                 continue
+                        except:
+                            continue
 
-            pbar.update(1)
+            # pbar.update(1)
 
-            if idx == target:
-                with open('entities.txt', 'w') as outfile:
-                    for ent_list in all_entities:
-                        for ent in ent_list:
-                            outfile.write(ent)
-                break
+            # if idx == target:
+            #     with open('entities.txt', 'w') as outfile:
+            #         for ent_list in all_entities:
+            #             for ent in ent_list:
+            #                 outfile.write(ent)
+            #     break
 
                     # text = soup.get_text()
                     # print(text)
