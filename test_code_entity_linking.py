ecc5befa2a3e02b2551aef7c1c062b63773a792e
WackyWabbit911 <dark.hunter005@gmail.com>
2021-11-17 15:39:24 +0100

    Working entity generation

--- test_code_entity_linking.py
+++ test_code_entity_linking.py
@@ -7,6 +7,10 @@ import pickle
 from elasticsearch import Elasticsearch
 import json
 import trident
+import nltk
+from nltk.corpus import wordnet as wn
+from textblob import TextBlob
+
 
 KEYNAME = "WARC-Record-ID"
 WARC_DIRECTORY = "data/warcs/"
@@ -69,20 +73,125 @@ def read_warc_files():
         #    entity_list = pickle.load(fp)
         exit(1)
 
-def search(query):
+def search(query,size):
     e = Elasticsearch("http://fs0.das5.cs.vu.nl:10010/")
-    p = { "query" : { "query_string" : { "query" : query }}}
+    p = { "query" : { "query_string" : { "query" : query } }, "size":size}
     response = e.search(index="wikidata_en", body=json.dumps(p))
     id_labels = []
     if response:
         for hit in response['hits']['hits']:
+            print()
+            print(hit)
+            print()
+            print("---------------------------------")
             label = hit['_source']['schema_name']
             id = hit['_id']
             #id_labels.setdefault(id, set()).add(label)
             id_labels.append(id)
     return id_labels
 
-def link_lookup(url):
+def get_nouns_from_definition(synsets):
+    definitions = [x.definition() for x in synsets]
+    return [ [ent.text.strip() for ent in nlp(definition).ents] for definition in definitions]
+
+
+## Using NLTK tokenization
+# def get_nouns_from_definition(synsets):
+#     definitions = [x.definition() for x in synsets]
+#     print(definitions)
+#     is_noun = lambda pos:pos[:2] == "NN"
+#
+#     return [ [word for (word,pos) in nltk.pos_tag(nltk.word_tokenize(definition)) if is_noun] for definition in definitions]
+
+## Using TextBlob tokenization.
+# def get_nouns_from_definition(synsets):
+#     definitions = [x.definition() for x in synsets]
+#     print(definitions)
+#     return [ TextBlob(definition).noun_phrases for definition in definitions]
+
+def order_entities(list_of_uris):
+
+    # First split on "/", then take the last part of the URI including the entity number.
+    # Afterwards, remove the Q and the ">" from the entity number to get the number itself
+    # Then convert everything to int so that it can be sorted according to the entity numbers
+
+    entity_numbers = [ int( uri.split("/")[-1][1:][:-1] ) for uri in list_of_uris]
+
+    return [x for _, x in sorted( zip (entity_numbers, list_of_uris  ) )]
+
+def entity_linking():
+
+    with open('entity_lists/list_of_entities_gua_1.txt', 'rb') as fp:
+        entity_list = pickle.load(fp)
+
+    for idx, entity in enumerate( entity_list ):
+
+        if idx > 50:
+            exit(1)
+
+        #check_entity = entity["Entity"]
+        check_entity = "Washington"
+        # Impossible: USA/US
+        print("++++++++++++++++++++")
+        print("Checking for Entity: ", check_entity)
+
+        # if any( x in check_entity for x in ["/", "[", "]", "(", ")", "%"] ):
+        #     print("Faulty entity, skipping")
+        #     print("++++++++++++++++++++")
+        #     continue
+
+        synsets = wn.synsets(check_entity, pos=wn.NOUN)[:5]
+
+        if synsets:
+            search_size = 10
+            synonyms = list( set( [lemma.name().replace("_", " ") for x in synsets for lemma in x.lemmas()] ) )
+            synonym_length = len(synonyms)
+
+            if synonym_length <= 1:
+                #print("NO SYNONYMS FOUND, CHECKING DEFINITION")
+                synonyms = list( set( get_nouns_from_definition(synsets)[0] ) )[:5]
+                search_size = 10
+
+            elif 2 < synonym_length <= 5:
+                #print("REDUCING SIZE!, TOO MANY SYNONYMS")
+                search_size = 10
+
+            elif synonym_length > 5:
+                #print("TOO MANY SYNONYMS, ONLY CHECKING FIRST 10")
+                synonyms = synonyms[:5]
+                search_size = 10
+
+            #synonyms += get_nouns_from_definition(synsets)[0][:5]
+
+            list_of_uris = []
+            for synonym in synonyms:
+                print("++++++++++++++++++++")
+                print("CHECKING FOR ENTITY: ", check_entity, " AND SYNONYM: ", synonym)
+                list_of_uris += search(synonym, search_size)
+                list_of_uris += search("(%s) AND (%s)" % (check_entity, synonym), search_size )
+                print("++++++++++++++++++++")
+
+
+        else:
+            print("No Synonyms,querying normally")
+            list_of_uris = search(check_entity,20)
+
+        list_of_uris = list ( set ( list_of_uris)) # For only obtaining the unique ones
+
+        ordered_uris = order_entities(list_of_uris)
+        if ordered_uris:
+            print("Entity: ", check_entity, " ;  Corresponding best URI: ", ordered_uris)
+            print("++++++++++++++++++++")
+            print()
+
+        else:
+            print("No page found for entity: ", check_entity, ".  Continuing search.")
+            print("++++++++++++++++++++")
+            print()
+
+        exit(1)
+
+#def link_lookup(url):
 
     # QUERIES FOR SPARQL, NOT NEEDED
     # query = "PREFIX wde: <http://www.wikidata.org/entity/> " \
@@ -90,44 +199,29 @@ def link_lookup(url):
     #         "PREFIX wdpn: <http://www.wikidata.org/prop/direct-normalized/> " \
     #             "select ?s where { ?s wdp:P31 wde:Q145 . } LIMIT 10"
 
-    #query = "PREFIX wde: <http://www.wikidata.org/entity/> " \
-    #        "SELECT DISTINCT ?v WHERE { ?v ?p wde:"+ entity + " . } "
-
+    # query = "PREFIX wde: <http://www.wikidata.org/entity/> " \
+    #         "SELECT DISTINCT ?v WHERE { ?v ?p wde:Q145 } "
 
     # Load the KB
-    db = trident.Db(KBPATH)
+    #db = trident.Db(KBPATH)
     # url = "<http://www.wikidata.org/entity/Q145>"
-    print("URL: ", url)
 
-    term_id = db.lookup_id(url)
-    print("Term id: ", term_id)
-    po = db.po(term_id) # returns relations, such as P31 or http://www.wikidata.org/prop/direct/P279
-    print(po)
-    relation = db.lookup_str(po[5][0])
+    #results = db.sparql(query)
 
-    print(relation)
+    #print(results)
 
-    exit(1)
 
-#results = db.sparql(query)
-def entity_linking():
-
-    with open('entity_lists/list_of_entities_gua_1.txt', 'rb') as fp:
-        entity_list = pickle.load(fp)
-
-    for entity in entity_list:
-        print("Checking for Entity: ", entity["Entity"])
-        list_of_urls = search(entity["Entity"])
-
-        if not list_of_urls:
-            print("No urls found in the wikidata, skipping entity: ", entity["Entity"])
-            continue
-
-        for url in list_of_urls:
-            print(list_of_urls)
-            link_lookup(url)
-
-        exit(1)
+    # print("URL: ", url)
+    #
+    # term_id = db.lookup_id(url)
+    # print("Term id: ", term_id)
+    # po = db.po(term_id) # returns relations, such as P31 or http://www.wikidata.org/prop/direct/P279
+    # print(po)
+    # relation = db.lookup_str(po[5][0])
+    #
+    # print(relation)
+    #
+    # exit(1)
 
 #read_warc_files()
 
