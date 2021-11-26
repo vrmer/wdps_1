fdfebe13e8bbbf9d351f1ae11cdbd28cb7b3eb6b
Marcell Fekete <marcell.fekete95@gmail.com>
2021-11-26 20:09:28 +0100

    pushed new run_entity_link_script

--- run_entity_link_script.sh
+++ run_entity_link_script.sh
@@ -6,6 +6,9 @@ echo "Downloading and Installing English Spacy Model 'en-core-web-md' ..."
 python3 -m spacy download en_core_web_md
 echo "Downloading NLTK datasets ..."
 python3 ./import_nltk_sets.py
+echo
+echo
+echo "------------"
 echo "Running script on default values"
 echo "Therefore, all the warc files will be read and saved and all ElasticSearch results will be generated and saved within the 'outputs' folder"
 echo "Please adjust the warc_archives argument in the config.ini file to the filepath or glob pattern you are trying to process though"
@@ -13,4 +16,7 @@ echo "If this script is ran once without error, you can keep on adjusting the co
 echo "If the WARC archives are already processed, set process_warc to False"
 echo "If the candidate results are already saved, set save_es_results to False"
 echo "If you want to test another model besides the prominence-based one, put in lesk, glove or bert"
+echo "------------"
+echo
+echo
 python3 ./starter_code.py
